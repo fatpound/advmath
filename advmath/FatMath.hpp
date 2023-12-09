60518c799e7759b9e048d8e95340cf407e9a0848
fatpound <fatpound@users.noreply.github.com>
2023-12-09 15:48:12 +0300

    simple concepts used on fatmath functions

--- advmath/FatMath.hpp
+++ advmath/FatMath.hpp
@@ -3,16 +3,20 @@
 #include <cmath>
 
 #include <numbers>
+#include <concepts>
 
 namespace fatpound::math
 {
     template <typename T>
-    inline auto sq(const T& x)
+    concept Number = std::is_integral_v<T> || std::is_floating_point_v<T>;
+
+    template <Number T>
+    inline T sq(const T& x)
     {
         return x * x;
     }
 
-    template <typename T>
+    template <std::floating_point T>
     inline T wrap_angle(T theta)
     {
         const T twoPi = static_cast<T>(2) * std::numbers::pi_v<T>;
